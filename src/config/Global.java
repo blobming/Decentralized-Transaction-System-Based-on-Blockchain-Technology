a32d0a5b914f986e6657b4a29d569a692e37cd30
ZhangxyCC <xinyanz.c@gmail.com>
2019-01-30 21:00:38 +0800

    edit global

--- src/config/Global.java
+++ src/config/Global.java
@@ -3,7 +3,8 @@ package config;
 public class Global {
 	private static Global global = new Global();
 	
-	public static int maxBlockTxNum = 8;
+	public static int maxBlockTxNum = 16;
+	public static int minBlockTxNum = 4;
 	public static Global getInstance() {
         return global;
     }
