0b8c4a246b655109a378f819284b8d8c05bfd5a9
blobming <xjm.good@gmail.com>
2019-01-30 21:38:36 +0800

    jiaming Wed 2019-1-30

--- src/obj/Block.java
+++ src/obj/Block.java
@@ -1,5 +1,40 @@
 package obj;
 
-public class Block {
+import java.util.Date;
 
+import config.Global;
+
+public class Block {
+	private int preHashCode; 	// hash of previous block
+	private int hashCode; 		// hash of current block
+	private int merkleRootHash; // hash of the root node of the merkleTree
+	private Date timestamp;
+	private int nBits;			// the difficulty of mining
+	private int nonce;			// Be used to calculate sactisfied hash
+	private BlockBody blockBody;
+	
+	public Block(int preHashCode, int merkleRootHash, int nonce) {
+		this.preHashCode = preHashCode;
+		this.merkleRootHash = merkleRootHash;
+		this.timestamp = new Date();
+		this.nBits = Global.nbits;
+		this.nonce = nonce;
+		this.hashCode = this.calculateHash();
+	}
+	
+	// use preHashCode, merkleRootHash, timestamp, 
+	// nBits and nounce to calculate the hash of current block
+	public int calculateHash() {
+		String content = "" + 
+						this.preHashCode +
+						this.merkleRootHash +
+						this.timestamp +
+						this.nBits +
+						this.nonce;
+		return content.hashCode();
+	}
+	
+	
+	
+	
 }
