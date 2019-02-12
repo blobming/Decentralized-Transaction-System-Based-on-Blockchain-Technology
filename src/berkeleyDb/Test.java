5b31d5f17b83d86301ebe1e4e4dbf0fee9cb5613
ZhangxyCC <xinyanz.c@gmail.com>
2019-02-12 13:46:48 +0800

    berkeleydb test

--- src/berkeleyDb/Test.java
+++ src/berkeleyDb/Test.java
@@ -4,18 +4,41 @@ import java.io.File;
 import com.sleepycat.je.*;
 
 public class Test {
-
+	private static Environment environment;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//同EnvironmentConfig来配置环境
+		//鍚孍nvironmentConfig鏉ラ厤缃幆澧�
 	    EnvironmentConfig environmentConfig=new EnvironmentConfig();
 	    environmentConfig.setTransactional(true);
 	    environmentConfig.setAllowCreate(true);
-	    //homeDirectory是数据库存放的目录
+	    //homeDirectory鏄暟鎹簱瀛樻斁鐨勭洰褰�
 	    
+	    environmentConfig = null;
 	  //Open Environment
-	    Environment environment=new Environment(new File("./"),environmentConfig);
+	    environment=new Environment(new File("./"),environmentConfig);
+	    
+	    String dbName = "test";
+	    DatabaseConfig dbconfig  = new DatabaseConfig();
+	    dbconfig.setAllowCreate(true);
+	    dbconfig.setTransactional(false);
+	    dbconfig.setSortedDuplicates(false);
+	    Database myClassDb = environment.openDatabase(null, "classDb", dbconfig);
+	   String key = "key1";
+	   String value = "value1";
+	   try {
+		   //设置key/value,注意DatabaseEntry内使用的是bytes数组
+		   DatabaseEntry theKey=new DatabaseEntry(key.getBytes("UTF-8"));
+		   DatabaseEntry theData=new DatabaseEntry(value.getBytes("UTF-8"));
+		   myClassDb.put(null, theKey, theData);
+		  } catch (Exception e) {
+		      // 错误处理
+		  }
+		  
+		 
+	    
+	    
+	    
 	}
 
 }
