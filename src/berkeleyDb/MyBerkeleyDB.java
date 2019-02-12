596f991f2b1cc40b911cf8f598212d22d50b41c6
blobming <xjm.good@gmail.com>
2019-02-12 21:34:09 +0800

    Db

--- src/berkeleyDb/MyBerkeleyDB.java
+++ src/berkeleyDb/MyBerkeleyDB.java
@@ -8,14 +8,19 @@ import com.sleepycat.je.*;
 import Utilities.Utilities;
 
 public class MyBerkeleyDB {
-	 private Environment environment;
-	 private Database database;    
-	 private String path;
-	 private String charset;
-	 public MyBerkeleyDB() {
-		 charset = "utf-8";
-	 }
-	 public void setPath(String path) {
+	private static MyBerkeleyDB instance = null;
+	private Environment environment;
+	private Database database;    
+	private String path;
+	private String charset;
+	private MyBerkeleyDB() {
+		charset = "utf-8";
+	}
+	public static MyBerkeleyDB GetInstance() {
+		if(instance == null) instance = new MyBerkeleyDB();
+		return instance;
+	}
+	public void setPath(String path) {
 		//判断path是否存在
 		File file = new File(path);
 		if(file.mkdir()){ 
@@ -25,73 +30,72 @@ public class MyBerkeleyDB {
 		} 
 		//确定存储路径
 		this.path = path;
-	 }
-	 public void setEnvironment(String path , long chacheSize){
-		 setPath(path); 
-		 //setChacheSize(chacheSize); 
-		 //配置环境
-		 EnvironmentConfig envConfig = new EnvironmentConfig(); 
-		 envConfig.setAllowCreate(true); 
-		 //envConfig.setCacheSize(this.chacheSize); 
-		 //创建环境
-		 environment = new Environment(new File(this.path),envConfig); 
-	 }
-	 //open database
-	 public void open(String dbName) { 
-		 DatabaseConfig dbConfig = new DatabaseConfig(); 
-		 dbConfig.setAllowCreate(true); 
-		 dbConfig.setSortedDuplicates(false); //不存储重复关键字
-		 this.database = environment.openDatabase(null, dbName, dbConfig); 
-	 }
-	 public void close()
-	 {
-        database.close();
-        environment.close();
-	 }
-	 public void put(String key,String value) {
-		 try {
-			 DatabaseEntry k = new DatabaseEntry(key.getBytes(charset)); 
-			 DatabaseEntry v = new DatabaseEntry(value.getBytes(charset)); 
-			 database.put(null, k, v); 
-		 }catch(Exception e) {
-			 e.printStackTrace();
-		 }
-	 }
-	 public String get(String key) throws UnsupportedEncodingException { 
-
-		 DatabaseEntry k = new DatabaseEntry(key.getBytes(charset)); 
-		 DatabaseEntry v = new DatabaseEntry(); 
-		 if(database.get(null, k, v, LockMode.DEFAULT) == OperationStatus.SUCCESS) {
-			 byte[] retData = v.getData();
-			 String res = new String(retData,charset);
-			 return res;
-		 }else {
-			 return null;
-		 }
-	 }
+	}
+	public void setEnvironment(String path){
+		setPath(path); 
+		//setChacheSize(chacheSize); 
+		//配置环境
+		EnvironmentConfig envConfig = new EnvironmentConfig(); 
+		envConfig.setAllowCreate(true); 
+		//envConfig.setCacheSize(this.chacheSize); 
+		//创建环境
+		environment = new Environment(new File(this.path),envConfig); 
+	}
+	//open database
+	public void open(String dbName) { 
+		DatabaseConfig dbConfig = new DatabaseConfig(); 
+		dbConfig.setAllowCreate(true); 
+		dbConfig.setSortedDuplicates(false); //不存储重复关键字
+		this.database = environment.openDatabase(null, dbName, dbConfig); 
+	}
+	public void close()
+	{
+	   database.close();
+	   environment.close();
+	}
+	public void put(String key,String value) {
+		try {
+			DatabaseEntry k = new DatabaseEntry(key.getBytes(charset)); 
+			DatabaseEntry v = new DatabaseEntry(value.getBytes(charset)); 
+			database.put(null, k, v); 
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public String get(String key) throws UnsupportedEncodingException { 
+		DatabaseEntry k = new DatabaseEntry(key.getBytes(charset)); 
+		DatabaseEntry v = new DatabaseEntry(); 
+		if(database.get(null, k, v, LockMode.DEFAULT) == OperationStatus.SUCCESS) {
+			byte[] retData = v.getData();
+			String res = new String(retData,charset);
+			return res;
+		}else {
+			return null;
+		}
+	}
 	 
-	 public ArrayList<String> getAllKey() { 
-		 ArrayList<String> result = new ArrayList<String>(); 
-		 Cursor cursor = database.openCursor(null, null); 
-		 DatabaseEntry key = new DatabaseEntry(); 
-		 DatabaseEntry value = new DatabaseEntry(); 
-		 while(cursor.getNext(key, value, LockMode.DEFAULT) == OperationStatus.SUCCESS) { 
-			 byte[] bytes = key.getData(); 
-			 if(bytes != null) { 
-				 result.add(new String(bytes)); 
-				 key = new DatabaseEntry(); 
-			 }else{
-				 key = new DatabaseEntry(); 
-			 }
-		 } 
-		 cursor.close(); 
-		 return result; 
-	 }
+	public ArrayList<String> getAllKey() { 
+		ArrayList<String> result = new ArrayList<String>(); 
+		Cursor cursor = database.openCursor(null, null); 
+		DatabaseEntry key = new DatabaseEntry(); 
+		DatabaseEntry value = new DatabaseEntry(); 
+		while(cursor.getNext(key, value, LockMode.DEFAULT) == OperationStatus.SUCCESS) { 
+			byte[] bytes = key.getData(); 
+			if(bytes != null) { 
+				result.add(new String(bytes)); 
+				key = new DatabaseEntry(); 
+			}else{
+				key = new DatabaseEntry(); 
+			}
+		} 
+		cursor.close(); 
+		return result; 
+	}
 	//按照键值删除数据
-    public void del(Object key){
-        DatabaseEntry k = new DatabaseEntry(Utilities.toByteArray(key));  //键值转化
-        //Object value = get(key);                                //获取值
-        database.removeSequence(null, k);                       //删除值
-        //return value;                                           //返回删除的值
-    }
+	public void del(Object key){
+	    DatabaseEntry k = new DatabaseEntry(Utilities.toByteArray(key));  //键值转化
+	    //Object value = get(key);                                //获取值
+	    database.removeSequence(null, k);                       //删除值
+	    //return value;                                           //返回删除的值
+	}
 }
--- src/berkeleyDb/Test.java
+++ src/berkeleyDb/Test.java
@@ -1,38 +1,27 @@
 package berkeleyDb;
-import java.io.File;
+import java.io.UnsupportedEncodingException;
 
 import com.sleepycat.je.*;
 
 public class Test {
-	private static Environment environment;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MyBerkeleyDB dbInstance = MyBerkeleyDB.GetInstance();
+		dbInstance.setEnvironment("./Data");
+		dbInstance.open("Test");
+		//dbInstance.put("123", "456");
+		dbInstance.put("2345", "789");
 		
-		/*
-		
-	    EnvironmentConfig environmentConfig=new EnvironmentConfig();
-	    environmentConfig.setTransactional(true);
-	    environmentConfig.setAllowCreate(true); //如果不存在则创建一个
-	    
-	//    environmentConfig = null;
-	  //Open Environment
-	    environment=new Environment(new File("./Data"),environmentConfig);
-	    
-	    String dbName = "test";
-	    */
-		Environment env =  BerkeleyDb.GetInstance().GetEnvironment();
-	    Database myClassDb = env.openDatabase(null, "classDb", BerkeleyDb.GetDbConfig());
-	    String key = "key1";
-	    String value = "value1";
-	   try {
-		   //设置key/value,注意DatabaseEntry内使用的是bytes数组
-		   DatabaseEntry theKey=new DatabaseEntry(key.getBytes("UTF-8"));
-		   DatabaseEntry theData=new DatabaseEntry(value.getBytes("UTF-8"));
-		   myClassDb.put(null, theKey, theData);
-		  } catch (Exception e) {
-		      // 错误处理
-		  }
-	    
+		for(String a:dbInstance.getAllKey()) {
+			System.out.println(a);
+			try {
+				System.out.println(dbInstance.get(a));
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		dbInstance.close();
 	}
 
 }
